fbb6df4cc53bf727b30f6dec73289b1ec77ff4fe
郭斌勇 <guobinyong@qq.com>
2019-12-11 20:11:13 +0800

    完善类型

--- src/conditionOperat.ts
+++ src/conditionOperat.ts
@@ -1,22 +1,37 @@
-//布尔条件的类型
+/**
+ * 布尔条件的类型
+ */
 type BoolCondition = Exclude<any, Function | Promise<any> | Array<any>>;
 
-//Promise条件的类型
+/**
+ * Promise条件的类型
+ */
 type PromCondition = Promise<Condition>
 
-//函数条件的类型
-type FunCondition = ()=>(BoolCondition | PromCondition)
+/**
+ * 函数条件的类型
+ */
+type FunCondition = ()=>(BoolCondition | PromCondition | ConditionSet)
 
-//条件的类型
+/**
+ * 条件的类型
+ */
 type Condition = BoolCondition | FunCondition | PromCondition
 
-//关系类型
+/**
+ * 关系类型
+ */
 type Relationship = "and" | "or"
 
-//布尔运算的结果的类型
-type LogicOperationResult = boolean | Promise<BoolCondition>
+/**
+ * 运算结果的类型
+ */
+type OperatedResult = boolean | Promise<BoolCondition>
 
-//带有关系，并含有多个条件的集合
+/**
+ * 条件集
+ * 带有关系，并含有多个条件的集合
+ */
 interface ConditionSet  extends Array<CondExpression>{
   /**
    * 各个条件表达式之间的关系；
@@ -30,7 +45,9 @@ interface ConditionSet  extends Array<CondExpression>{
   not?:boolean;
 }
 
-//条件表达式
+/**
+ * 条件表达式的类型
+ */
 type CondExpression = ConditionSet | Condition
 
 
@@ -38,8 +55,23 @@ type CondExpression = ConditionSet | Condition
  * 条件运算
  * 对一系列条件进行逻辑运算；
  * @param condExpress : CondExpression   条件表达式
+ * @return OperatedResult 返回布尔 或者 Promise
+ *
+ *
+ * 特性：
+ * - 可以指定条件条件表达式的间的逻辑关系：与、或、非；
+ * - 条件表达式可以任意层级嵌套，即：条件集 可以 嵌套 条件集；
+ * - 短路运算
+ *    在对条件表达式进行运算的过程中，如果运算中途已经能够确认最终的运算结果，则便会停止对剩余表达式的计算，并返回计算结果；
+ *
+ * - 简单优先
+ *    为了提高运算效率，除了加入了短路运算的特性外，还加入了简单优先的计算原则，即：对于同一层级表达式，会按照下面的顺序优先计算：
+ *    1. BoolCondition : 除了函数、Promise、ConditionSet（即：数组）以外的所有其它数据类型的条件表达式，这些条件会被当作布尔值来计算；
+ *    2. FunCondition : 函数类型的条件表达式；
+ *    3. ConditionSet : 条件集类型 的条件表达式，即 数组类型；
+ *    4. PromCondition : Promise类型的条件表达式；
  */
-export function conditionOperat(condExpress:CondExpression):LogicOperationResult {
+export function conditionOperat(condExpress:CondExpression):OperatedResult {
 
   if (Array.isArray(condExpress)){
     var condSet:ConditionSet = condExpress as ConditionSet
@@ -56,6 +88,11 @@ export function conditionOperat(condExpress:CondExpression):LogicOperationResult
 
     //先对计算 不是 数组 和 不是 Promise 的 条件进行计算
     let orRes = condSet.some(function (condExp) {
+
+      if (typeof condExp === "function"){
+        condExp = condExp()
+      }
+
       if (!condExp){
         return false
       }
@@ -66,12 +103,6 @@ export function conditionOperat(condExpress:CondExpression):LogicOperationResult
         return false
       }
 
-      if (typeof condExp === "function"){
-        condExp = condExp()
-        if (!(condExp instanceof Promise)){
-          return condExp
-        }
-      }
 
       //先跳过 Promise 类型
       if (condExp instanceof Promise){
@@ -135,6 +166,10 @@ export function conditionOperat(condExpress:CondExpression):LogicOperationResult
 
     //先对计算 不是 数组 和 不是 Promise 的 条件进行计算
     let andRes = condSet.every(function (condExp) {
+      if (typeof condExp === "function"){
+        condExp = condExp()
+      }
+
       if (!condExp){
         return false
       }
@@ -145,12 +180,6 @@ export function conditionOperat(condExpress:CondExpression):LogicOperationResult
         return true
       }
 
-      if (typeof condExp === "function"){
-        condExp = condExp()
-        if (!(condExp instanceof Promise)){
-          return condExp
-        }
-      }
 
       //先跳过 Promise 类型
       if (condExp instanceof Promise){
