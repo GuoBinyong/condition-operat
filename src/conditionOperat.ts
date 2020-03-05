35a41101e79b56478423e9e0206631497c455383
郭斌勇 <guobinyong@qq.com>
2020-03-05 22:24:59 +0800

    完成注释

--- src/conditionOperat.ts
+++ src/conditionOperat.ts
@@ -15,6 +15,29 @@
 
 
 
+
+
+
+/**
+ * 判断目标是否是对象类型
+ * @param target : any   目标对象
+ *
+ * 仅通过 target instanceof Object 判断是不行的，因为 对于 Object.create(null) 创建的对象 通过 ` Object.create(null) instanceof Object ` 来判断 返回的是 false
+ * 即：通过 Object.create(null) 创建的对象是不被 instanceof  认为是继续于 Object 的
+ * 
+ * typeof null 也返回 "object"
+ */
+function isObject(target:any):boolean {
+  // return target instanceof Object || typeof target === "object"
+  var tarType = typeof target;
+  return  target && (tarType === "object" || tarType === "function");
+}
+  
+  
+
+
+
+
 /**
  * 非运算表达式
  */
@@ -22,28 +45,47 @@ interface NotExpression {
   /**
    * 对原来的值取反
    */
-  not?:boolean
+  not?:boolean;
 }
 
 
 /**
  * NotExpression 的类型守卫
- * @param condExp : CondExpression 表达式
  */
-function isNotExpression(condExp:CondExpression<any,any>): condExp is NotExpression {
-  return isObject(condExp)
+function isNotExpression(expr:any): expr is NotExpression {
+  return isObject(expr)
 }
 
 
 /**
  * 映射条件类型
+ * 表示 在 条件映射对象 ConditionMap 中，条件表达式对应的 key
  */
 type MapCondition = string | number | (String & NotExpression) | (Number & NotExpression) | symbol 
 
-function isMapCondition(condExp:any):  condExp is  MapCondition {
-  let condType = typeof condExp
-  return condType === "string" || condType === "number" || condType === "symbol" || condExp instanceof String || condExp instanceof Number
+/**
+ * MapCondition 的类型守卫
+ */
+function isMapCondition(cond:any):  cond is  MapCondition {
+  let condType = typeof cond;
+  return condType === "string" || condType === "number" || condType === "symbol" || cond instanceof String || cond instanceof Number
 }
+
+
+
+/**
+ * 对象条件类型
+ * 表示那些除  函数条件 FunCondition 、异步条件 PromCondition 外 的 对象；当这些对象被作为 条件 时，会将其valueOf()方法返回的值转换成 布尔值，然后作 布尔条件 来用
+ */
+interface ObjCondition extends NotExpression{}
+
+/**
+ * ObjCondition 的类型守卫
+ */
+function isObjCondition(cond:any): cond is ObjCondition {
+  return cond && cond instanceof Object && !(isConditionSet(cond) || isFunCondition(cond) || isPromCondition(cond))
+}
+
 
 
 
@@ -57,10 +99,9 @@ type BoolCondition = boolean | undefined | null | Boolean | ObjCondition
 
 /**
  * BoolCondition 的类型守卫
- * @param condExp : CondExpression 表达式
  */
-function isBoolCondition(condExp:any): condExp is BoolCondition {
-  return typeof condExp === "boolean" || condExp == undefined || condExp instanceof Boolean || isObjCondition(condExp)
+function isBoolCondition(cond:any): cond is BoolCondition {
+  return typeof cond === "boolean" || cond == undefined || cond instanceof Boolean || isObjCondition(cond)
 }
 
 
@@ -74,10 +115,9 @@ interface PromCondition<ThisValue,Args> extends Promise<CondExpression<ThisValue
 
 /**
  * PromCondition 的类型守卫
- * @param condExp : CondExpression 表达式
  */
-function isPromCondition<ThisValue,Args>(condExp:any): condExp is PromCondition<ThisValue,Args> {
-  return condExp instanceof Promise
+function isPromCondition<ThisValue,Args>(cond:any): cond is PromCondition<ThisValue,Args> {
+  return cond instanceof Promise
 }
 
 
@@ -87,25 +127,16 @@ function isPromCondition<ThisValue,Args>(condExp:any): condExp is PromCondition<
  * 带有逻辑的函数，会对其返回值反复地进行条件运算，直到计算到得到 布尔结果 为止；
  */
 interface FunCondition<ThisValue,Args> extends NotExpression {
-  (this:ThisValue,...args:Args extends any[] ? Args : []):CondExpression<any,any>;
-}
-
-
-
-interface ObjCondition extends NotExpression{}
-
-function isObjCondition(cond:any): cond is ObjCondition {
-  return cond && cond instanceof Object && !(isConditionSet(cond) || isFunCondition(cond) || isPromCondition(cond))
+  (this:ThisValue,...args:Args extends any[] ? Args : []):CondExpression<ThisValue,Args>;
 }
 
 
 
 /**
  * FunCondition 的类型守卫
- * @param condExp : CondExpression 表达式
  */
-function isFunCondition<ThisValue,Args>(condExp:any): condExp is FunCondition<ThisValue,Args> {
-  return typeof condExp === "function"
+function isFunCondition<ThisValue,Args>(cond:any): cond is FunCondition<ThisValue,Args> {
+  return typeof cond === "function"
 }
 
 
@@ -117,10 +148,12 @@ function isFunCondition<ThisValue,Args>(condExp:any): condExp is FunCondition<Th
  */
 type Condition<ThisValue,Args> = BoolCondition | FunCondition<ThisValue,Args> | PromCondition<ThisValue,Args> | MapCondition
 
+/**
+ * Condition 的类型守卫
+ */
 function isCondition<ThisValue,Args>(cond:any): cond is Condition<ThisValue,Args> {
   return isFunCondition<ThisValue,Args>(cond) || isPromCondition<ThisValue,Args>(cond) || isMapCondition(cond) || isBoolCondition(cond)
 }
-
 
 
 
@@ -129,10 +162,6 @@ function isCondition<ThisValue,Args>(cond:any): cond is Condition<ThisValue,Args
  */
 type Relationship = "and" | "or"
 
-/**
- * 运算结果的类型
- */
-type OperatedResult = boolean | Promise<boolean>
 
 /**
  * 条件集
@@ -149,10 +178,9 @@ interface ConditionSet<ThisValue,Args>  extends Array<CondExpression<ThisValue,A
 
 /**
  * ConditionSet 的类型守卫
- * @param condExp : CondExpression 表达式
  */
-function isConditionSet<ThisValue,Args>(condExp:any): condExp is ConditionSet<ThisValue,Args> {
-  return Array.isArray(condExp)
+function isConditionSet<ThisValue,Args>(expr:any): expr is ConditionSet<ThisValue,Args> {
+  return Array.isArray(expr)
 }
 
 
@@ -165,9 +193,11 @@ function isConditionSet<ThisValue,Args>(condExp:any): condExp is ConditionSet<Th
 type CondExpression<ThisValue,Args> = ConditionSet<ThisValue,Args> | Condition<ThisValue,Args>
 
 
-
-function isCondExpression<ThisValue,Args>(condExp:any): condExp is CondExpression<ThisValue,Args> {
-  return isConditionSet<ThisValue,Args>(condExp) || isCondition<ThisValue,Args>(condExp)
+/**
+ * CondExpression 的类型守卫
+ */
+function isCondExpression<ThisValue,Args>(expr:any): expr is CondExpression<ThisValue,Args> {
+  return isConditionSet<ThisValue,Args>(expr) || isCondition<ThisValue,Args>(expr)
 }
 
 
@@ -191,28 +221,45 @@ type NotSequence = NotValue[]
 
 /**
  * 对 target 做一系列连续的 非操作，
- * @param target : any    操作的目标，会直接将其作为布尔值来对待
+ * @param target : any    操作的目标，如果 target 有 valueof() 方法，会将 valueof() 方法返回的值作为布尔值来对象，否则，会直接将 target 作为布尔值来对待
  * @param notSequ : NotSequence    指示非操作序列的数组
  * @return boolean     非操作后的结果
  */
 function notOperat(target:any,notSequ:NotSequence):boolean {
   return notSequ.reduce(function (res,not) {
     return not && not.valueof() ? !res : res;
-  },Boolean(target && target.valueof()))
+  },Boolean(target && target.valueof ? target.valueof() : target) )
 }
 
 
+/**
+ * flatCondition 将表达式扁平化处理之后的结果类型
+ */
+type FlatResult<ThisValue,Args> = {
+  expr:Exclude<CondExpression<ThisValue,Args>, MapCondition | FunCondition<ThisValue,Args>>,
+  notSequ:NotSequence
+};
+
 
 /**
- * 扁平化 函数条件
- * @param condExp : CondExpression   条件表达式；如果 condExp 不是函数条件，则不作处理 返回 condExp 自身
- * @param thisValue ?:  any   可选；函数条件的 this 的值
- * @param args ?:any[]      可选；函数条件的 参数
- *
- * 函数条件可能还会返回函数条件，返回的函数条件可能还会返回函数条件，可以无休止地这样延续下去；
- * 本方法的作用就是对函数条件进行运算，直到返回的不是函数条件为止
+ * 递归条件
+ * 表示 需要通过 运算才能得到真正的条件，并且运算后得到的条件 可能还是个 与原来相同类型的 条件
+ * 
+ * 注意：
+ * - 因为 异步条件 PromCondition 通过运算后返回的不会再是异步条件，所以 异步条件 PromCondition 不是递归条件；
+ * - 但是 异步条件 PromCondition 通过运算后返回的条件 可能会是一个再次返回 异步条件的 条件；比如：异步条件 反应一个 函数条件，而该函数条件返回的又是一个异步条件；
  */
-function flatCondition<ThisValue,Args>(operatOptions:OperatOptions<ThisValue,Args>): {expr:Exclude<CondExpression<ThisValue,Args>, MapCondition | FunCondition<ThisValue,Args>>,notSequ:NotSequence} {
+type RecursiveCondition<ThisValue,Args> = MapCondition | FunCondition<ThisValue,Args>
+
+
+/**
+ * 扁平化 递归条件 RecursiveCondition
+ * @param operatOptions : OperatOptions   运算选项类型
+ *
+ * 函数条件 FunCondition 可能还会返回 函数条件 FunCondition，返回的 函数条件 FunCondition 可能还会返回 函数条件 FunCondition，可以无休止地这样延续下去；映射条件 MapCondition 也是；像这样的条件称为 递归条件
+ * 本方法的作用就是对递归条件进行运算，直到返回的不是递归条件为止
+ */
+function flatCondition<ThisValue,Args>(operatOptions:OperatOptions<ThisValue,Args>): FlatResult<ThisValue,Args> {
 
   let {expr,"this":thisValue,args,notSequ,...condMap} = operatOptions;
 
@@ -250,23 +297,120 @@ function flatCondition<ThisValue,Args>(operatOptions:OperatOptions<ThisValue,Arg
 
 
 
+
+
+
+
+
+
+
 /**
- * 判断目标是否是对象类型
- * @param target : any   目标对象
- *
- * 仅通过 target instanceof Object 判断是不行的，因为 对于 Object.create(null) 创建的对象 通过 ` Object.create(null) instanceof Object ` 来判断 返回的是 false
- * 即：通过 Object.create(null) 创建的对象是不被 instanceof  认为是继续于 Object 的
- * 
- * typeof null 也返回 "object"
+ * 条件选项类型
  */
-function isObject(target:any):boolean {
-// return target instanceof Object || typeof target === "object"
-var tarType = typeof target;
-return  target && (tarType === "object" || tarType === "function");
+interface ExpressionOptions<ThisValue,Args> {
+  expr?:CondExpression<ThisValue,Args>,   //条件表达式
+  this?:ThisValue,    //设置条件表达式中 函数条件 的 this 的值
+
+  /**
+   * 设置条件表达式中 函数条件 的 参数序列；即该参数是个数组，里面包含传递给 条件函数 的参数
+   * 
+   * 注意：
+   * thisValue 和 args 会被应用到所有的 函数条件，包括那些 运算过程 中产生的函数条件，比如：函数条件返回的函数条件、异步条件决议时传递出的 函数条件
+   */
+  args?:Args,   
+  
+  /**
+   * 是否要对表达式进行全量运算，默认是短路计算；全量计算会对条件集中的每个条件进行计算；
+   * 
+   * # 全景运算
+   * 会对表达式中的所有条件依次且完全地进行运算
+   * 
+   * # 短路运算
+   * 在对条件表达式进行运算的过程中，如果运算中途已经能够确认最终的运算结果，则便会停止对剩余表达式的计算，并返回计算结果；
+   *
+   * ## 简单优先
+   * 为了提高运算效率，除了加入了短路运算的特性外，还加入了简单优先的计算原则，即：对于同一层级表达式，会按照下面的顺序优先计算：
+   * 1. BoolCondition | RecursiveCondition: 布尔条件 BoolCondition 和 扁平化后 布尔条件 BoolCondition 的 递归条件 RecursiveCondition；
+   * 2. ConditionSet : 条件集；
+   * 3. PromCondition : 异步条件；
+   */
+  full?:boolean,
+
+  /* 
+  非值的序列
+  如果存在该属性值，则忽略 expr 属性的 表达式的 not 属性
+  */
+ notSequ?:NotSequence
+}
+
+/**
+ * ExpressionOptions 的类型守卫
+ */
+function isExpressionOptions<ThisValue,Args>(opts:any): opts is ExpressionOptions<ThisValue,Args> {
+  return opts && (opts.expr || opts.this || opts.args || opts.full || opts.notSequ)
 }
 
 
 
+
+
+
+
+/**
+ * 条件映射类型；条件Map
+ */
+/* type ConditionMap<ThisValue, Args> = {
+  [key in string | number | symbol]: CondExpression<ThisValue, Args>
+} */
+
+
+/**
+ * 条件映射对象的类型
+ * 映射条件 可通过 该类型的对象来查找其对应的条件；
+ */
+interface ConditionMap<ThisValue, Args>  {
+  [prop : MapCondition]: CondExpression<ThisValue, Args>;
+}
+
+/**
+ * ConditionMap 的类型守卫
+ */
+function isConditionMap<ThisValue,Args>(condMap:any): condMap is ConditionMap<ThisValue,Args> {
+  return isObject(condMap)
+}
+
+
+
+
+
+
+/**
+ * 条件运算的选项的类型
+ */
+type OperatOptions<ThisValue,Args> = ExpressionOptions<ThisValue,Args> & ConditionMap<ThisValue, Args> 
+
+/**
+ * OperatOptions 的类型守卫
+ */
+function isOperatOptions<ThisValue,Args>(opts:any): opts is OperatOptions<ThisValue,Args> {
+  return isConditionMap<ThisValue,Args>(opts) || isExpressionOptions<ThisValue,Args>(opts)
+}
+
+
+
+
+
+
+
+/**
+ * 运算结果的类型
+ */
+type OperatedResult = boolean | Promise<boolean>
+
+
+/**
+ * conditionOperat 接口
+ */
 export interface conditionOperat {
   create<ThisValue,Args>(...operatOptions:OperatOptions<ThisValue,Args>[]): (...operatOptions:OperatOptions<ThisValue,Args>[])=>OperatedResult;
   create<ThisValue,Args>(condExpress:CondExpression<ThisValue,Args>,...operatOptions:OperatOptions<ThisValue,Args>[]): (...operatOptions:OperatOptions<ThisValue,Args>[])=>OperatedResult;
@@ -276,34 +420,29 @@ export interface conditionOperat {
 
 
 
-
-
 /**
  * 条件运算
  * 对一系列条件进行逻辑运算；
- * @param condExpress : CondExpression   条件表达式
- * @param thisValue ？: any   设置条件表达式中 函数条件 的 this 的值
- * @param args ？: any[]   设置条件表达式中 函数条件 的 参数序列； 即该参数是个数组，里面包含传递给 条件函数 的参数
- *
- * @return OperatedResult 返回布尔类型 或者 返回布尔类型的Promise类型 的值
  * 
- * 注意：
- * thisValue 和 args 会被应用到所有的 函数条件，包括那些 运算过程 中产生的函数条件，比如：函数条件返回的函数条件、异步条件决议时传递出的 函数条件
- *
- *
- * 特性：
- * - 可以指定条件条件表达式的间的逻辑关系：与、或、非；
- * - 条件表达式可以任意层级嵌套，即：条件集 可以 嵌套 条件集；
- * - 短路运算
- *    在对条件表达式进行运算的过程中，如果运算中途已经能够确认最终的运算结果，则便会停止对剩余表达式的计算，并返回计算结果；
- *
- * - 简单优先
- *    为了提高运算效率，除了加入了短路运算的特性外，还加入了简单优先的计算原则，即：对于同一层级表达式，会按照下面的顺序优先计算：
- *    1. BaseCondition | FunCondition: 除了 异步条件 PromCondition、条件集 ConditionSet 以外的所有其它数据类型的条件表达式，这些条件会被当作布尔值来计算；
- *    2. ConditionSet : 条件集；
- *    3. PromCondition : 异步条件；
+ * @returns OperatedResult   返回 OperatedResult 类型的结果，即 布尔类型 或者 返回布尔类型的 Promise 类型 的值；
+ * 只有依靠 异步条件 才能决定最后的运算结果时，conditionOperat 才会返回 Promise 类型的 异步运算结果 
  */
+
+
+ /**
+  * 接口1
+  * conditionOperat<ThisValue,Args>(...operatOptions:OperatOptions<ThisValue,Args>[]):OperatedResult
+  * @param operatOptions : OperatOptions  conditionOperat 接收一系列关于条件的选项对象
+  * 
+  */
 export function conditionOperat<ThisValue,Args>(...operatOptions:OperatOptions<ThisValue,Args>[]):OperatedResult;
+
+/**
+ * 接口2
+ * @param condExpress : CondExpression   条件表达式
+ * @param operatOptions : OperatOptions   一系列关于条件的选项对象
+ * 
+ */
 export function conditionOperat<ThisValue,Args>(condExpress:CondExpression<ThisValue,Args>,...operatOptions:OperatOptions<ThisValue,Args>[]):OperatedResult;
 export function conditionOperat<ThisValue,Args>(exprOrOptions:CondExpression<ThisValue,Args>|OperatOptions<ThisValue,Args>,...operatOptions:OperatOptions<ThisValue,Args>[]):OperatedResult {
   
@@ -326,6 +465,14 @@ export function conditionOperat<ThisValue,Args>(exprOrOptions:CondExpression<Thi
 }
 
 
+ /**
+  * 以短路的方式进行条件运算
+  * conditionOperatForFast<ThisValue,Args>(operatOptions:OperatOptions<ThisValue,Args>):OperatedResult
+  * @param operatOptions : OperatOptions  关于条件的选项对象
+  * 
+  * @returns OperatedResult   返回 OperatedResult 类型的结果，即 布尔类型 或者 返回布尔类型的 Promise 类型 的值；
+  * 只有依靠 异步条件 才能决定最后的运算结果时，conditionOperat 才会返回 Promise 类型的 异步运算结果 
+  */
 function conditionOperatForFast<ThisValue,Args>(operatOptions:OperatOptions<ThisValue,Args>):OperatedResult {
   
   let finalOperOpts = {...operatOptions};
@@ -536,6 +683,15 @@ function conditionOperatForFast<ThisValue,Args>(operatOptions:OperatOptions<This
 
 
 
+
+ /**
+  * 以全量的方式进行条件运算
+  * conditionOperatForFull<ThisValue,Args>(operatOptions:OperatOptions<ThisValue,Args>):OperatedResult
+  * @param operatOptions : OperatOptions  关于条件的选项对象
+  * 
+  * @returns OperatedResult   返回 OperatedResult 类型的结果，即 布尔类型 或者 返回布尔类型的 Promise 类型 的值；
+  * 只有依靠 异步条件 才能决定最后的运算结果时，conditionOperat 才会返回 Promise 类型的 异步运算结果 
+  */
 function conditionOperatForFull<ThisValue,Args>(operatOptions:OperatOptions<ThisValue,Args>):OperatedResult {
   
   let finalOperOpts = {...operatOptions};
@@ -667,73 +823,6 @@ function conditionOperatForFull<ThisValue,Args>(operatOptions:OperatOptions<This
 
 
 
-/* 
-条件map
-条件选项
-表达式选项
-
-export function conditionOperat<ThisValue,Args>(condOptions):OperatedResult;
-export function conditionOperat<ThisValue,Args>(condExpress:CondExpression<ThisValue,Args>,condMapOrOptions):OperatedResult;
-export function conditionOperat<ThisValue,Args>(condExpress:CondExpression<ThisValue,Args>,condMap):OperatedResult;
-export function conditionOperat<ThisValue,Args>(condExpress:CondExpression<ThisValue,Args>,condOptions,condMap):OperatedResult;
-export function conditionOperat<ThisValue,Args>(condExpress:CondExpression<ThisValue,Args>,condOptions):OperatedResult;
- */
-
-
-
-
-
-/**
- * 条件选项类型
- */
-interface ExpressionOptions<ThisValue,Args> {
-  expr?:CondExpression<ThisValue,Args>,   //条件表达式
-  this?:ThisValue,    //设置条件表达式中 函数条件 的 this 的值
-  args?:Args,    //设置条件表达式中 函数条件 的 参数序列；即该参数是个数组，里面包含传递给 条件函数 的参数
-  full?:boolean,  //是否要对表达式进行全量计划，默认是短路计算；全量计算会对条件集中的每个条件进行计算；
-
-  /* 
-  非值的序列
-  如果存在该属性值，则忽略 expr 属性的 表达式的 not 属性
-  */
- notSequ?:NotSequence
-}
-
-function isExpressionOptions<ThisValue,Args>(opts:any): opts is ExpressionOptions<ThisValue,Args> {
-  return opts && (opts.expr || opts.this || opts.args || opts.full || opts.notSequ)
-}
-
-
-
-/**
- * 条件映射类型；条件Map
- */
-/* type ConditionMap<ThisValue, Args> = {
-  [key in string | number | symbol]: CondExpression<ThisValue, Args>
-} */
-
-
-
-interface ConditionMap<ThisValue, Args>  {
-  [prop : MapCondition]: CondExpression<ThisValue, Args>;
-}
-
-
-function isConditionMap<ThisValue,Args>(opts:any): opts is ConditionMap<ThisValue,Args> {
-  return isObject(opts)
-}
-
-
-
-/**
- * 运算选项类型
- */
-type OperatOptions<ThisValue,Args> = ExpressionOptions<ThisValue,Args> & ConditionMap<ThisValue, Args> 
-
-function isOperatOptions<ThisValue,Args>(opts:any): opts is OperatOptions<ThisValue,Args> {
-  return isConditionMap<ThisValue,Args>(opts) || isExpressionOptions<ThisValue,Args>(opts)
-}
-
 
 
 
@@ -742,11 +831,22 @@ function isOperatOptions<ThisValue,Args>(opts:any): opts is OperatOptions<ThisVa
 
 /**
  * 创建快捷运算函数
- * 快捷运算函数 是带有一部分参数值，只接收剩余参数 的条件运算函数；
- *
- * 创建指定条件表达式的快捷运算函数
+ * 快捷运算函数 是带有默认选项的条件运算函数；
  */
+
+  /**
+  * 接口1
+  * create<ThisValue,Args>(...operatOptions:OperatOptions<ThisValue,Args>[]): (...operatOptions:OperatOptions<ThisValue,Args>[])=>OperatedResult
+  * @param operatOptions : OperatOptions  conditionOperat 接收一系列关于条件的选项对象
+  */
 export function create<ThisValue,Args>(...operatOptions:OperatOptions<ThisValue,Args>[]): (...operatOptions:OperatOptions<ThisValue,Args>[])=>OperatedResult;
+
+/**
+ * 接口2
+ * create<ThisValue,Args>(condExpress:CondExpression<ThisValue,Args>,...operatOptions:OperatOptions<ThisValue,Args>[]): (...operatOptions:OperatOptions<ThisValue,Args>[])=>OperatedResult
+ * @param condExpress : CondExpression   条件表达式
+ * @param operatOptions : OperatOptions   一系列关于条件的选项对象
+ */
 export function create<ThisValue,Args>(condExpress:CondExpression<ThisValue,Args>,...operatOptions:OperatOptions<ThisValue,Args>[]): (...operatOptions:OperatOptions<ThisValue,Args>[])=>OperatedResult;
 export function create<ThisValue,Args>(exprOrOptions:CondExpression<ThisValue,Args>|OperatOptions<ThisValue,Args>,...operatOptions:OperatOptions<ThisValue,Args>[]){
 
